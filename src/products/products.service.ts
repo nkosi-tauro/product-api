965354118f230a81e1fec6bcf7fa8f73779dbc1a
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-05 12:14:30 +0200

    udpate service with product model

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,7 +1,12 @@
 import { Injectable } from "@nestjs/common";
 
-
+import {Product } from './products.model'
 @Injectable()
 export class ProductsService {
-    
+    products: Product[] = [];
+
+    insertProduct(title:string, description:string, price:number){
+        const newProduct = new Product(new Date().toString(), title, description, price)
+        this.products.push(newProduct)
+    }
 }
