5445e47aacdd66642f822bcba753d320309c889d
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-05 12:30:47 +0200

    add uuid

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,15 @@
 import { Injectable } from "@nestjs/common";
-
+import {v4 as uuidv4} from 'uuid';
 import {Product } from './products.model'
+
 @Injectable()
 export class ProductsService {
     products: Product[] = [];
 
     insertProduct(title:string, description:string, price:number){
-        const newProduct = new Product(new Date().toString(), title, description, price)
+        const prodId = uuidv4()
+        const newProduct = new Product(prodId, title, description, price)
         this.products.push(newProduct)
+        return prodId
     }
 }
