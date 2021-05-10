674ae3d6274940893a27ba3aec36ec38725c057b
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-10 12:37:01 +0200

    add NestConfig to parse env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
+import { ConfigModule } from '@nestjs/config';
+
 
 @Module({
-  imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://nkosi-tauro:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/productsApi?retryWrites=true&w=majority`)],
+  imports: [
+    ConfigModule.forRoot(),
+    ProductsModule, 
+    MongooseModule.forRoot(`mongodb+srv://nkosi-tauro:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/productsApi?retryWrites=true&w=majority`)
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
