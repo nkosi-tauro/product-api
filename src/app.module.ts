71e4dc7bef02b23b8a1ec13ab0b7fd21a7587ecc
nkosi-tauro <nkosilati23@yahoo.com>
2021-05-10 11:54:08 +0200

    add mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, MongooseModule.forRoot(`mongodb+srv://nkosi-tauro:${process.env.DB_PASS}@vuejwtauth.mpdvx.mongodb.net/productsApi?retryWrites=true&w=majority`)],
   controllers: [AppController],
   providers: [AppService],
 })
